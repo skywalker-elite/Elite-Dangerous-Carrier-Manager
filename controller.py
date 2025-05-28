9a38e4b74cb76241b8c4a8d48b6de4e2d194642d
skywalker-elite <197877581+skywalker-elite@users.noreply.github.com>
2025-05-28 19:19:47 -0400

    Fixed an error causing embed ping to fail

--- controller.py
+++ controller.py
@@ -107,7 +107,7 @@ class CarrierController:
             if self.settings.get('notifications', 'jump_plotted_discord'):
                 title = f'{self.model.get_name(carrierID)} ({self.model.get_callsign(carrierID)})'
                 description = f'Jump plotted to **{self.model.get_destination_system(carrierID, use_custom_name=True)}** body **{self.model.get_destination_body(carrierID)}**, arriving {self.model.get_departure_hammer_countdown(carrierID)}'
-                self.webhook_handler.send_message_with_embed(title, description, self.settings.get('notifications', 'jump_plotted_discord_ping'))
+                self.webhook_handler.send_message_with_embed(title=title, description=description, ping=self.settings.get('notifications', 'jump_plotted_discord_ping'))
         elif status_new == 'cool_down':
             # jump completed
             # print(f'{self.model.get_name(carrierID)} ({self.model.get_callsign(carrierID)}) has arrived at {self.model.get_current_system(carrierID)} body {self.model.get_current_body(carrierID)}')
@@ -118,7 +118,7 @@ class CarrierController:
             if self.settings.get('notifications', 'jump_completed_discord'):
                 title = f'{self.model.get_name(carrierID)} ({self.model.get_callsign(carrierID)})'
                 description = f'Jump completed at **{self.model.get_current_system(carrierID, use_custom_name=True)}** body **{self.model.get_current_body(carrierID)}**'
-                self.webhook_handler.send_message_with_embed(title, description, self.settings.get('notifications', 'jump_completed_discord_ping'))
+                self.webhook_handler.send_message_with_embed(title=title, description=description, ping=self.settings.get('notifications', 'jump_completed_discord_ping'))
         elif status_new == 'cool_down_cancel':
             # jump cancelled
             # print(f'{self.model.get_name(carrierID)} ({self.model.get_callsign(carrierID)}) cancelled a jump')
@@ -129,7 +129,7 @@ class CarrierController:
             if self.settings.get('notifications', 'jump_cancelled_discord'):
                 title = f'{self.model.get_name(carrierID)} ({self.model.get_callsign(carrierID)})'
                 description = f'Jump cancelled'
-                self.webhook_handler.send_message_with_embed(title, description, self.settings.get('notifications', 'jump_cancelled_discord_ping'))
+                self.webhook_handler.send_message_with_embed(title=title, description=description, ping=self.settings.get('notifications', 'jump_cancelled_discord_ping'))
         elif status_new == 'idle' and status_old in ['cool_down', 'cool_down_cancel']:
             # cool down complete
             # print(f'{self.model.get_name(carrierID)} ({self.model.get_callsign(carrierID)}) has finished cool down and is ready to jump')
@@ -140,7 +140,7 @@ class CarrierController:
             if self.settings.get('notifications', 'cooldown_finished_discord'):
                 title = f'{self.model.get_name(carrierID)} ({self.model.get_callsign(carrierID)})'
                 description = f'Cool down complete, ready to jump'
-                self.webhook_handler.send_message_with_embed(title, description, self.settings.get('notifications', 'cooldown_finished_discord_ping'))
+                self.webhook_handler.send_message_with_embed(title=title, description=description, ping=self.settings.get('notifications', 'cooldown_finished_discord_ping'))
 
     def play_sound(self, sound_file:str, block:bool=False):
         if path.exists(sound_file):
